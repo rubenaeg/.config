ce3457a14b5eddb9da2737df2918f6ca1a5c02bb
ruben <email@aegerter.tech>
2021-11-08 18:42:29 +0100

    :sparkles: Add custom nvim behaviour for closing and switching the buffer

--- nvim/lua/_nvim-tree.lua
+++ nvim/lua/_nvim-tree.lua
@@ -1,5 +1,38 @@
+-- Credit to https://stackoverflow.com/a/7615129
+local function splitString (input, seperator)
+  if seperator == nil then
+    seperator = "%s"
+  end
+
+  local split = {}
+
+  for str in string.gmatch(input, "([^"..seperator.."]+)") do
+    table.insert(split, str)
+  end
+
+  return split
+end
+
+-- Custom function to open NvimTree
+function OpenNvimTree ()
+  local currentBuffer = vim.api.nvim_buf_get_name(0)
+  print(currentBuffer)
+  if currentBuffer == nil or currentBuffer == '' then
+    vim.api.nvim_command('NvimTreeOpen')
+  end
+
+  local currentBufferArr = splitString(currentBuffer, '/')
+  if currentBufferArr[#currentBufferArr] == 'NvimTree' then
+    -- Switch to previous buffer
+    vim.api.nvim_command('bprevious')
+  else
+    vim.api.nvim_command('NvimTreeFindFile')
+  end
+end
+
 -- Shortcuts
-vim.api.nvim_set_keymap('', 'ff', ':NvimTreeFindFile<cr>', {})
+vim.api.nvim_set_keymap('', 'ff', ':lua OpenNvimTree() <cr>', {})
+vim.api.nvim_set_keymap('', 'fc', ':NvimTreeClose<cr>', {})
 
 require('nvim-tree').setup({
   auto_close = true,
