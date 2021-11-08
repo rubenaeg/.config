bd5eb2623c0fd210e24d326a0d58a5cf4d0c9cfc
ruben <email@aegerter.tech>
2021-11-08 18:37:01 +0100

    :art: Improve colorscheme configuration

--- nvim/lua/_colorscheme.lua
+++ nvim/lua/_colorscheme.lua
@@ -10,3 +10,14 @@ require('nebulous').setup({
     variables  = false,
   },
 })
+
+--[[
+vim.g.material_style = 'darker'
+require('material').setup({
+  disable = {
+		background = true, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
+		term_colors = false, -- Prevent the theme from setting terminal colors
+		eob_lines = false -- Hide the end-of-buffer lines
+	},
+})
+]]--
