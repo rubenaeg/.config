c4f9f00acca625e3cb05e2f972a77da829afa283
Ruben <email@aegerter.tech>
2022-07-14 21:56:02 +0200

    :sparkles: Close Neovim when NvimTree is left in buffer

--- nvim/lua/_nvim-tree.lua
+++ nvim/lua/_nvim-tree.lua
@@ -33,13 +33,22 @@ function OpenNvimTree()
   end
 end
 
+-- Autoclose Neovim when tree is the last buffer
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
+
 -- Shortcuts
 vim.api.nvim_set_keymap('n', 'ff', ':lua OpenNvimTree() <cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', 'fc', ':NvimTreeClose<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', 'F', ':NvimTreeClose<cr>', {noremap = true})
 
 require('nvim-tree').setup({
-  auto_close = true,
   hijack_cursor = true,
   update_focused_file = {enable = true},
   view = {
